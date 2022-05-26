467de30633b51f59a56cca0b21ad4ac3cbbd5552
Dimon <ha020285mdv@gmail.com>
2022-05-26 19:56:28 +0300

    create new todo

--- js/script.js
+++ js/script.js
@@ -21,45 +21,55 @@ submit.addEventListener("click", function() {
         completed: completed
     };
 
-    Poster(data_to_post);
+    poster(data_to_post);
 });
 
 
-async function Poster(data) {
+async function poster(data) {
     let response = await fetch('http://localhost:8080/todo', {
         method: 'POST',
         headers: {'Content-Type': 'application/json;charset=utf-8'},
         body: JSON.stringify(data)
     });
 
-      let result = await response.json();
-      
-      print(result)
+    let result = await response.json();
+    
+    print(result);
 }
 
 
+const add = document.getElementById("new");
+add.addEventListener("click", creator(2, 'new', 'new text', true));
 
 
+function creator(id, title, body, completed) {
+    newToDo = document.createElement("div");
+    newToDo.classList.add("todo");
+    newToDo.dataset.id = id;
+    
+    newTitle = document.createElement("h2");
+    newTitle.innerText = title;
+    newTitle.classList.add("title");
+    newToDo.appendChild(newTitle);
+        
+    newBody = document.createElement("div");
+    newBody.innerText = body;
+    newBody.classList.add("body");
+    newToDo.appendChild(newBody);
+        
+    newCompleted = document.createElement("div");
+    newCompleted.classList.add("completed");
+    newToDo.appendChild(newCompleted);
 
-const getDataFromServer = () => {
-    return new Promise((resolve, reject) => { // (0)
-        setTimeout(() => {
-            resolve(server); // успех
-            // reject(new Error('500 ServerError')); // не успех
-        }, 2000);
-    })
+    newLabel = document.createElement("label");
+    newLabel.innerText = "Is completed";
+    newCompleted.appendChild(newLabel);
+
+    newInput = document.createElement("input");
+    newInput.type = "checkbox";
+    newInput.checked = completed;
+    newLabel.appendChild(newInput);    
+
+    content = document.getElementById("content");
+    content.appendChild(newToDo);
 }
-
-function asyncAction() {
-    const responce = getDataFromServer();
-    responce
-        .then((data) => {  // (1)
-            console.log('data: ', data); // [{ name: 'Vlad', ... }, { name: 'Vova', ... }]
-        })
-        .catch((err) => { // (2)
-            console.log('err: ', err);
-        })
-}
-
-
-btn.addEventListener('click', asyncAction)
